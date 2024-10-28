6033ecbf7715cfb9982cf9cca47118b40e656ab3
MARehman75 <rehman.hanif16@gmail.com>
2024-10-28 16:15:00 +0500

    Second Commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"]
 }));
